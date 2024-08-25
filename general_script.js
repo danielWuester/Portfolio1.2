1c9efede954abbeb630d3bc0bcca71c7932a6a37
Daniel Wuester <daniel.wuester@hftm.ch>
2024-08-25 17:56:20 +0200

    Kontakt

--- general_script.js
+++ general_script.js
@@ -9,6 +9,19 @@ if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
 }
 }
 
+// mein Alter für Text berechnen
+const geburtsdatum = new Date('1976-06-28');
+const heute = new Date();
+
+let alter = heute.getFullYear() - geburtsdatum.getFullYear();
+const monat = heute.getMonth() - geburtsdatum.getMonth();
+
+if(monat < 0 || (monat === 0 && heute.getDate() < geburtsdatum.getDate())) {
+  alter--;
+}
+
+document.getElementById('age').textContent = alter;
+
 // Wenn der Benutzer auf den Button klickt, wird nach oben gescrollt
 function topFunction() {
   document.body.scrollTop = 0; // Für Safari
